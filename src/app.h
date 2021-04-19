b4da2bc03774b61f7eb8e4309ff201187a9de668
rallen <rallen@gmail.com>
2021-04-18 20:31:02 -0700

    clean up keycode handling

--- src/app.h
+++ src/app.h
@@ -22,6 +22,7 @@ class App {
     void update();
     void cleanup();
     void resize(unsigned width, unsigned height);
+    int symToPitch(SDL_Keycode sym);
 
     AppWindow *mAppWindow;
     AppGL *mAppGL;
